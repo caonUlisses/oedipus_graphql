710cdf79780cbda49ad3f010fec37f6c5f3a21ec
Ulisses Caon <ulissescaon@gmail.com>
2018-02-14 15:17:47 -0200

    Make user resolver methods compatible with acync

--- src/graphql/resolvers/user_resolver.js
+++ src/graphql/resolvers/user_resolver.js
@@ -2,21 +2,21 @@ import User from './../../models/users'
 
 const UserResolver = {
   Query: {
-    users: () => {
+    users: async () => {
       return User.find({})
     },
-    user: (root, { _id }) => {
+    user: async (root, { _id }) => {
       return User.findById(_id)
     }
   },
   Mutation: {
-    create: (root, { name, email, password, picture, access }) => {
+    create: async (root, { name, email, password, picture, access }) => {
       return new User({name, email, password, picture, access}).save()
     },
-    update: (root, { _id, name, email, picture, access }) => {
+    update: async (root, { _id, name, email, picture, access }) => {
       return User.findByIdAndUpdate(_id, { name, email, picture, access })
     },
-    delete: (root, { _id }) => {
+    delete: async (root, { _id }) => {
       return User.findByIdAndRemove(_id)
     }
   }
