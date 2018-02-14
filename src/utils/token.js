0440e919fcfad72605507913d10e47cf1e13b966
Ulisses Caon <ulissescaon@gmail.com>
2018-02-14 12:36:34 -0200

    :hammer: Refactor defineAuthTokens parameters

--- src/utils/token.js
+++ src/utils/token.js
@@ -3,10 +3,10 @@ import config from './../config/master'
 
 const key = config.app.keys.models
 
-const defineAuthTokens = async (receiver = this, field, field2, issuer = 'server') => {
+const defineAuthTokens = async (receiver = this, info, props, issuer = 'server') => {
   try {
     const expiration = Date.now() + 2000000000
-    const token = await jwt.sign({ _id: receiver._id.toHexString(), field, field2, expiration, issuer }, key).toString()
+    const token = await jwt.sign({ _id: receiver._id.toHexString(), info, props, expiration, issuer }, key).toString()
     await receiver.tokens.push(token)
 
     return { token }
