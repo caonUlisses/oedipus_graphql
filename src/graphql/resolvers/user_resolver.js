155c062df3f589f03d14b95398498973a45010b1
Ulisses Caon <ulissescaon@gmail.com>
2018-02-15 21:25:01 -0200

    Add try catch blocks to resolver

--- src/graphql/resolvers/user_resolver.js
+++ src/graphql/resolvers/user_resolver.js
@@ -5,29 +5,41 @@ const UserResolver = {
   Query: {
     users: async (root, args, { user }) => {
       if (!user) { throw new Error('Faça login primeiro') }
-      return User.find({}, { password: 0 })
+      try {
+        return User.find({}, { password: 0 })
+      } catch (error) { throw new Error(error) }
     },
     user: async (root, { _id }, { user }) => {
       if (!user) { throw new Error('Faça login primeiro') }
-      return User.findById(_id, { password: 0 })
+      try {
+        return User.findById(_id, { password: 0 })
+      } catch (error) { throw new Error(error) }
     }
   },
   Mutation: {
     create: async (root, { name, email, password, picture, access }) => {
-      const user = await new User({name, email, password, picture, access}).save()
-      return pick(user, ['name', 'email', 'picture'])
+      try {
+        const user = await new User({name, email, password, picture, access}).save()
+        return pick(user, ['name', 'email', 'picture'])
+      } catch (error) { throw new Error(error) }
     },
     update: async (root, { name, email, picture, access }, { user }) => {
       if (!user) { throw new Error('Faça login primeiro') }
-      return User.findOneAndUpdate({ _id: user._id }, { name, email, picture, access })
+      try {
+        return User.findOneAndUpdate({ _id: user._id }, { name, email, picture, access })
+      } catch (error) { throw new Error(error) }
     },
     delete: async (root, args, { user }) => {
-      return User.findByIdAndRemove(user._id)
+      try {
+        return User.findByIdAndRemove(user._id)
+      } catch (error) { throw new Error(error) }
     },
     login: async (root, { email, password }) => {
-      const user = await User.findOne({ email })
-      if (!user) throw new Error('Email não cadastrado')
-      return user.login(password, user)
+      try {
+        const user = await User.findOne({ email })
+        if (!user) throw new Error('Email não cadastrado')
+        return user.login(password, user)
+      } catch (error) { throw new Error(error) }
     }
   }
 }
